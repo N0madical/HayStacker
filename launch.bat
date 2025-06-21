63384666ac9d851a75f6e69f2ac129e5cacbdb3e
Nomadical <nomadicalyt@gmail.com>
2025-06-21 18:44:00 -0500

    add anisette

--- launch.bat
+++ launch.bat
@@ -1,16 +1,17 @@
 @echo off
 
-WHERE git
+WHERE sqlite3
 if %ERRORLEVEL% NEQ 0 (
     echo --
-    echo Please install Git:
-    echo https://git-scm.com/downloads/win
+    echo Please install SQlite database management:
+    echo https://sqlite.org/download.html
+    echo Direct download for x64 windows: https://sqlite.org/2025/sqlite-dll-win-x64-3500100.zip
     echo --
     pause
 ) 
 
-if not exist anisette-v3-server\ (
-    git clone https://github.com/Dadoum/anisette-v3-server
+if not exist .\reports.db (
+    sqlite3 reports.db "CREATE TABLE reports (id_short TEXT, timestamp INTEGER, datePublished TEXT, lat INTEGER, lon INTEGER, link TEXT, statusCode INTEGER, conf INTEGER)"
 )
 
 cd .\App
